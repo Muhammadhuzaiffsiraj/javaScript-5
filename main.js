34e76e7f47a1496411e57dd729e90dca11344a66
Muhammadhuzaiffsiraj <huzaiffasiraj884@gmail.com>
2025-01-04 23:21:27 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-let array = ['Apple','banana']
+let items = ['Apple', 'banana']
 
 function add(str) {
-   array.push(str); 
-    
+
+   if (!items.includes(str)) {
+      items.push(str);
+   }
 }
 add()
 
 function remove(str) {
-    array.pop();    
- }
- remove();
-
- function lastChar(str) {
-    return str.charAt(str.length - 1);
+   let array = items.indexOf(str);
+   if (array !== -1) {
+      items.splice(array, 1);
+   }
 }
+remove();
 
+function lastChar(str) {
+   if (str.length === 0) {
+      return null;
+   }
+   return str[str.length - 1];
+}
 
 function reverse(str) {
-    return str.split('').reverse().join('');
+   return str.split('').reverse().join('');
 }
 
-console.log(array);
+console.log(items);
+console.log(lastChar('Apple'));
+console.log(lastChar('Banana'));
+console.log(reverse(reverse('APPLE')));
+console.log(reverse('APPLE'));
+console.log(reverse(reverse('Banana')));
+console.log(reverse('Banana'));
 console.log(lastChar('Apple')); 
 console.log(lastChar('Banana')); 
 console.log(reverse(reverse('APPLE')));  
